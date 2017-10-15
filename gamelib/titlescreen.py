126d898ddc356b2e931bb61edcf6e39a93a49f08
Thiago Chaves de Oliveira Horta <shundread@gmail.com>
2017-10-15 18:11:40 +0300

    syntax error fix oops.

--- gamelib/titlescreen.py
+++ gamelib/titlescreen.py
@@ -4,8 +4,8 @@ def handle_key(game, data, event):
     print("Titlescreen: key pressed {0}".format(event))
 
 def simulate(game, data, dt):
-    title = data["title"]:
-    title["frame"] += dt
+    tdata = data["title"]
+    tdata["frame"] += dt
 
 def render(data):
     pygame.display.get_surface().fill((0, 0, 255))
