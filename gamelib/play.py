52ed5a825015bfbddc06d490ca8d8d779eafad32
Thiago Chaves de Oliveira Horta <shundread@gmail.com>
2017-10-20 00:40:22 +0300

    Prevents player from passing through walls

--- gamelib/play.py
+++ gamelib/play.py
@@ -17,6 +17,13 @@ ShadowLength = 400
 ColorDoor = (140, 110, 0)
 ColorWall = (128, 128, 128)
 ColorWindow = (0, 0, 255)
+ColorHead = (200, 200, 0)
+ColorShirt = (30, 200, 40)
+
+# Size info
+PlayerHead = 4
+PlayerShoulder = 4
+PlayerSize = 12
 
 def reset_data():
     return {
@@ -45,6 +52,10 @@ def init():
 
 def generate_map(data):
     mapgenerator.generate_map(data)
+    generate_minimap()
+
+def generate_minimap():
+    pass
 
 def handle_key(game, data, event):
     if (event.key == pygame.K_ESCAPE):
@@ -52,14 +63,45 @@ def handle_key(game, data, event):
     if (event.key == pygame.K_m):
         generate_map(data)
 
-def simulate(game, data, dt):
-    data["miliseconds"] += dt
+def simulate(game, game_data, dt):
+    game_data["miliseconds"] += dt
     keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_w]: data["position"]["y"] -= 1
-    if keys[pygame.K_s]: data["position"]["y"] += 1
-    if keys[pygame.K_a]: data["position"]["x"] -= 1
-    if keys[pygame.K_d]: data["position"]["x"] += 1
+    player_position = (px, py) = get_player_position(game_data)
+    (dx, dy) = (0, 0)
+    # TODO make speed adjustable to dt, and scale player position in the getter
+    if keys[pygame.K_w]: dy -= 1
+    if keys[pygame.K_s]: dy += 1
+    if keys[pygame.K_a]: dx -= 1
+    if keys[pygame.K_d]: dx += 1
+
+    area_map = game_data["map"]
+    buildings = area_map["buildings"]
+    line_barriers = buildings["doors"] + buildings["walls"]
+    prect = pygame.rect.Rect(0, 0, PlayerSize, PlayerSize)
+    for (x0, y0, x1, y1) in line_barriers:
+        lrect = line_to_rect(x0, y0, x1, y1)
+
+        # Collide horizontal
+        prect.center = (px + dx, py)
+        collision = rects_collision(prect, lrect)
+        dx = math.copysign(abs(dx) - collision[0], dx)
+
+        # Collide vertical
+        prect.center = (px, py + dy)
+        collision = rects_collision(prect, lrect)
+        dy = math.copysign(abs(dy) - collision[1], dy)
+    set_player_position(game_data, (int(px + dx), int(py + dy)))
+
+def line_to_rect(x0, y0, x1, y1):
+    x = min(x0, x1)
+    y = min(y0, y1)
+    w = max(x0, x1) - x
+    h = max(y0, y1) - y
+    return pygame.rect.Rect(x-2, y-2, w+4, h+4)
+
+def rects_collision(r1, r2):
+    return r1.clip(r2).size
 
 def render(data):
     # Renders the real world
@@ -69,12 +111,13 @@ def render(data):
 
     mouse_offset = (mdx, mdy) = get_mouse_offset()
     mouse_angle = get_mouse_angle(mdx, mdy)
-    player_position = (px, py) = (data["position"]["x"], data["position"]["y"])
+    player_position = (px, py) = get_player_position(data)
     camera = (cx, cy) = (
-        px + (0.3 * mdx - 0.5 * Width),
-        py + (0.3 * mdy - 0.5 * Height)
+        px + int(0.3 * mdx - 0.5 * Width),
+        py + int(0.3 * mdy - 0.5 * Height)
     )
-    drawrect(realworld, White, camera, (px - 2, py - 2, 5, 5))
+
+    drawcharacter(realworld, camera, player_position, mouse_angle)
 
     # Renders the player and the light surface
     light = resources["light"]
@@ -113,9 +156,15 @@ def render(data):
     light.set_alpha(ShadowAlpha)
     realworld.blit(light, (0, 0))
 
+    # TODO: cpu-friendly funky vision updates
+
+    # Renders the realworld (or the vision, if we got that far with the project)
     screen = pygame.display.get_surface()
     pygame.transform.scale(realworld, screen.get_size(), screen)
     pygame.display.flip()
+
+def drawcircle(surface, color, (cx, cy), (x, y), radius):
+    pygame.draw.circle(surface, color, (x - cx, y - cy), radius)
 
 def drawrect(surface, color, (cx, cy), (x, y, w, h)):
     pygame.draw.rect(surface, color, ((x - cx, y - cy), (w, h)))
@@ -126,6 +175,20 @@ def drawline(surface, color, (cx, cy), (x0, y0), (x1, y1), width):
 def drawpoly(surface, color, (cx, cy), points):
     transposed = [(x - cx, y - cy) for (x, y) in points]
     pygame.draw.polygon(surface, color, transposed)
+
+def drawcharacter(surface, camera, player_position, angle):
+    (px, py) = player_position
+    shoulder_left = (
+        px + int(4 * math.sin(angle)),
+        py - int(4 * math.cos(angle)),
+    )
+    shoulder_right = (
+        px - int(4 * math.sin(angle)),
+        py + int(4 * math.cos(angle)),
+    )
+    drawcircle(surface, ColorShirt, camera, shoulder_left, 4)
+    drawcircle(surface, ColorShirt, camera, shoulder_right, 4)
+    drawcircle(surface, ColorHead, camera, player_position, 4)
 
 def cast_shadow(surface, color, camera, (lx, ly), (x1, y1, x2, y2)):
     dy = y1 - ly
@@ -157,3 +220,10 @@ def get_mouse_offset():
 
 def get_mouse_angle(dx, dy):
     return math.atan2(dy, dx)
+
+def get_player_position(game_data):
+    return (game_data["position"]["x"], game_data["position"]["y"])
+
+def set_player_position(game_data, (x, y)):
+    game_data["position"]["x"] = x
+    game_data["position"]["y"] = y
