5d00eaef841bddcb8029173910dfd1ea64a97c92
Thiago Chaves de Oliveira Horta <shundread@gmail.com>
2017-10-19 22:42:51 +0300

    Stretches the view towards the mouse, ditches the flashlight concept (for now)

--- gamelib/play.py
+++ gamelib/play.py
@@ -8,12 +8,15 @@ Black = (0, 0, 0)
 White = (255, 255, 255)
 Size = (Width, Height) = (200, 200)
 Pass = 0.40
-HalfConeAngle = 0.15 * math.pi
-Shadow = (255, 0, 0)
-ShadowLength = 300
+HalfConeAngle = 0.20 * math.pi
+Shadow = White
+ShadowAlpha = 255
+ShadowLength = 400
 
 # Thing colors
 ColorDoor = (140, 110, 0)
+ColorWall = (128, 128, 128)
+ColorWindow = (0, 0, 255)
 
 def reset_data():
     return {
@@ -64,51 +67,58 @@ def render(data):
     realworld.fill(Black)
     area_map = data["map"]
 
+    mouse_offset = (mdx, mdy) = get_mouse_offset()
+    mouse_angle = get_mouse_angle(mdx, mdy)
     player_position = (px, py) = (data["position"]["x"], data["position"]["y"])
-    camera = (cx, cy) = (px - Width * 0.5, py - Height * 0.5)
-    mouse_angle = get_mouse_angle()
+    camera = (cx, cy) = (
+        px + (0.3 * mdx - 0.5 * Width),
+        py + (0.3 * mdy - 0.5 * Height)
+    )
+    drawrect(realworld, White, camera, (px - 2, py - 2, 5, 5))
 
-    # Renders the light surface
+    # Renders the player and the light surface
     light = resources["light"]
-    light.fill(White)
-    drawpoly(light, Black, camera, [
-        player_position,
-        (
-            px + math.cos(mouse_angle + HalfConeAngle) * ShadowLength,
-            py + math.sin(mouse_angle + HalfConeAngle) * ShadowLength,
-        ),
-        (
-            px + math.cos(mouse_angle - HalfConeAngle) * ShadowLength,
-            py + math.sin(mouse_angle - HalfConeAngle) * ShadowLength,
-        ),
-    ])
-
+    light.fill(Black)
+    # drawpoly(light, Black, camera, [
+    #     player_position,
+    #     (
+    #         px + math.cos(mouse_angle + HalfConeAngle) * ShadowLength,
+    #         py + math.sin(mouse_angle + HalfConeAngle) * ShadowLength,
+    #     ),
+    #     (
+    #         px + math.cos(mouse_angle - HalfConeAngle) * ShadowLength,
+    #         py + math.sin(mouse_angle - HalfConeAngle) * ShadowLength,
+    #     ),
+    # ])
 
     for room in area_map["buildings"]["floors"]:
         (x, y, w, h, r, g, b) = room
-        pygame.draw.rect(realworld, (r, g, b), ((x - cx, y - cy), (w, h)))
+        drawrect(realworld, (r, g, b), camera, (x, y, w, h))
 
     for wall in area_map["buildings"]["walls"]:
         (x0, y0, x1, y1) = wall
-        drawline(realworld, White, camera, (x0, y0), (x1, y1), 3)
-        cast_shadow(light, camera, player_position, wall, Shadow)
+        drawline(realworld, ColorWall, camera, (x0, y0), (x1, y1), 5)
+        cast_shadow(light, Shadow, camera, player_position, wall)
 
     for door in area_map["buildings"]["doors"]:
         (x0, y0, x1, y1) = door
-        #pygame.draw.line(realworld, ColorDoor, (x0 - px, y0 - py), (x1 - px, y1 - py), 3)
-        #cast_shadow(light, center, wall, Shadow)
+        drawline(realworld, ColorDoor, camera, (x0, y0), (x1, y1), 5)
+        cast_shadow(light, Shadow, camera, player_position, door)
 
     for window in area_map["buildings"]["windows"]:
         (x0, y0, x1, y1) = window
-        #pygame.draw.line(realworld, (0, 0, 255), (x0 - px, y0 - py), (x1 - px, y1 - py), 3)
+        drawline(realworld, ColorWindow, camera, (x0, y0), (x1, y1), 5)
 
     # Apply the light to the surface
-    light.set_alpha(128)
+    light.set_alpha(ShadowAlpha)
     realworld.blit(light, (0, 0))
 
     screen = pygame.display.get_surface()
     pygame.transform.scale(realworld, screen.get_size(), screen)
     pygame.display.flip()
+
+def drawrect(surface, color, (cx, cy), (x, y, w, h)):
+    pygame.draw.rect(surface, color, ((x - cx, y - cy), (w, h)))
 
 def drawline(surface, color, (cx, cy), (x0, y0), (x1, y1), width):
     pygame.draw.line(surface, color, (x0 - cx, y0 - cy), (x1 - cx, y1 - cy), width)
@@ -117,7 +127,7 @@ def drawpoly(surface, color, (cx, cy), points):
     transposed = [(x - cx, y - cy) for (x, y) in points]
     pygame.draw.polygon(surface, color, transposed)
 
-def cast_shadow(surface, camera, (lx, ly), (x1, y1, x2, y2), color):
+def cast_shadow(surface, color, camera, (lx, ly), (x1, y1, x2, y2)):
     dy = y1 - ly
     dx = x1 - lx
     angle1 = math.atan2(dy, dx)
@@ -137,10 +147,13 @@ def handle_swap(game):
     init()
     reload(mapgenerator)
 
-def get_mouse_angle():
+def get_mouse_offset():
     screen = pygame.display.get_surface()
     (w, h) = screen.get_size()
     (x, y) = pygame.mouse.get_pos()
     dx = x - (w * 0.5)
     dy = y - (h * 0.5)
+    return (dx, dy)
+
+def get_mouse_angle(dx, dy):
     return math.atan2(dy, dx)
